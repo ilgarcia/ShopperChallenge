4c86f621904ca0351b8c8e04ac60bcd4cf21f34b
ilgarcia <60369262+ilgarcia@users.noreply.github.com>
2023-05-15 21:50:18 -0300

    Adcionado banco de dados

--- server/database/sequelize.ts
+++ server/database/sequelize.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize(
   "challengeshopper",
   "root",
-  "Shopp&er767",
+  process.env.MYSQL_SERVER_PASSWORD,
   {
     host: "localhost",
     dialect: "mysql",
